fd63dd010919484e46263861b968403140facd84
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2024-05-30 21:09:24 +0600

    ilim

--- scr/main/news.js
+++ scr/main/news.js
@@ -32,28 +32,28 @@ export const getNews = (app) => {
     }
   });
 
-  //   app.patch("/update-news", async (req, res) => {
-  //     try {
-  //       const { _id, img, title, description, link, date } = req.body;
-  //       if (!_id) {
-  //         return res.status(400).json({ error: "News ID is required" });
-  //       }
-  //       const newsData = await news.findOne({ _id: req.body_id });
-  //       if (!newsData) {
-  //         return res.status(404).json({ error: "News not found" });
-  //       }
-  //       newsData._id = _id || newsData._id;
-  //       newsData.img = img || newsData.img;
-  //       newsData.title = title || newsData.title;
-  //       newsData.description = description || newsData.description;
-  //       newsData.link = link || newsData.link;
-  //       newsData.date = date || newsData.date;
+  app.patch("/update-news", async (req, res) => {
+    try {
+      const { _id, img, title, description, link, date } = req.body;
+      if (!_id) {
+        return res.status(400).json({ error: "News ID is required" });
+      }
+      const newsData = await news.findOne({ _id: req.body_id });
+      if (!newsData) {
+        return res.status(404).json({ error: "News not found" });
+      }
+      newsData._id = _id || newsData._id;
+      newsData.img = img || newsData.img;
+      newsData.title = title || newsData.title;
+      newsData.description = description || newsData.description;
+      newsData.link = link || newsData.link;
+      newsData.date = date || newsData.date;
 
-  //       await newsData.save();
-  //       res.status(200).json({ message: "News updated successfully" });
-  //     } catch (error) {
-  //       console.error("Error updating news:", error);
-  //       res.status(500).json({ error: "Internal Server Error" });
-  //     }
-  //   });
+      await newsData.save();
+      res.status(200).json({ message: "News updated successfully" });
+    } catch (error) {
+      console.error("Error updating news:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
 };
