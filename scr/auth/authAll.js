1ed5364ae11d6f5b55bfba2cff2a0172f81d86e0
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2024-05-30 18:01:45 +0600

    ilim

--- scr/auth/authAll.js
+++ scr/auth/authAll.js
@@ -1,6 +1,7 @@
 import User from "../model/order.js";
 import validator from "validator";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import { authenticate } from "../token/authenticate.js";
 
 const jwtToken = (id) => {
@@ -10,7 +11,7 @@ const jwtToken = (id) => {
 export const auth = (app) => {
   app.post("/register", async (req, res) => {
     try {
-      const { email, confirm_password, password } = req.body;
+      const { email, password, confirm_password } = req.body;
       if (!email || !validator.isEmail(email)) {
         return res.status(400).json({ error: "Некорректный email" });
       }
@@ -28,7 +29,8 @@ export const auth = (app) => {
           .status(409)
           .json({ error: "Пользователь с таким email уже существует" });
       }
-      const newUser = new User({ email, password });
+      const hashedPassword = await bcrypt.hash(password, 10); 
+      const newUser = new User({ email, password: hashedPassword });
       await newUser.save();
 
       res.status(201).json({
@@ -45,7 +47,7 @@ export const auth = (app) => {
     try {
       const { email, password } = req.body;
       const user = await User.findOne({ email });
-      if (!user || user.password !== password) {
+      if (!user || !(await bcrypt.compare(password, user.password))) {
         return res.status(401).json({ error: "Invalid username or password" });
       }
       const token = jwtToken(user._id);
@@ -60,31 +62,30 @@ export const auth = (app) => {
       res.status(500).json({ error: "Internal server error" });
     }
   });
-
-//   app.patch("/new_password", authenticate, async (req, res) => {
-//     try {
-//       const { old_password, password, confirm_password } = req.body;
-//       if (!password || !validator.isLength(password, { min: 8 })) {
-//         return res
-//           .status(400)
-//           .json({ error: "Пароль должен содержать не менее 8 символов" });
-//       }
-//       if (password !== confirm_password) {
-//         return res.status(400).json({ error: "Пароли не совпадают" });
-//       }
-//       const user = await User.findOne({ _id: req.user._id });
-//       if (!user || user.password !== old_password) {
-//         return res.status(401).json({ error: "Неверный старый пароль" });
-//       }
-//       user.password = password;
-//       await user.save();
-//       res.status(200).json({
-//         response: true,
-//         message: "Пароль успешно обновлен",
-//       });
-//     } catch (error) {
-//       console.error("Ошибка обновления пароля:", error);
-//       res.status(500).json({ error: "Внутренняя ошибка сервера" });
-//     }
-//   });
+  //   app.patch("/new_password", authenticate, async (req, res) => {
+  //     try {
+  //       const { old_password, password, confirm_password } = req.body;
+  //       if (!password || !validator.isLength(password, { min: 8 })) {
+  //         return res
+  //           .status(400)
+  //           .json({ error: "Пароль должен содержать не менее 8 символов" });
+  //       }
+  //       if (password !== confirm_password) {
+  //         return res.status(400).json({ error: "Пароли не совпадают" });
+  //       }
+  //       const user = await User.findOne({ _id: req.user._id });
+  //       if (!user || user.password !== old_password) {
+  //         return res.status(401).json({ error: "Неверный старый пароль" });
+  //       }
+  //       user.password = password;
+  //       await user.save();
+  //       res.status(200).json({
+  //         response: true,
+  //         message: "Пароль успешно обновлен",
+  //       });
+  //     } catch (error) {
+  //       console.error("Ошибка обновления пароля:", error);
+  //       res.status(500).json({ error: "Внутренняя ошибка сервера" });
+  //     }
+  //   });
 };
