fc4a29ed1a9eff85ba39756c569615c812266f70
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2024-05-30 13:01:46 +0600

    ilim

--- scr/personal/personal-cabinet.js
+++ scr/personal/personal-cabinet.js
@@ -12,13 +12,10 @@ export const personal = (app) => {
   app.get("/personal-cabinet", async (req, res) => {
     try {
       const user = await User.findOne({ _id: req.user._id });
-      if (!user) {
-        return res.status(404).json({ error: "User not found" });
-      }
       res.status(200).json({ user });
     } catch (error) {
       console.error("Error fetching user:", error);
-      res.status(500).json({ error: "Internal server erroryyyy" });
+      return res.status(404).json({ error: "User not found" });
     }
   });
 };
