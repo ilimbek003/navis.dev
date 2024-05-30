512fcef7b237fcb07c721a66a287de6122b3f58f
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2024-05-30 12:36:15 +0600

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { auth } from "./scr/auth/authAll.js";
-import { app } from "./scr/personal/personal-cabinet.js";
+import { get } from "./scr/personal/personal-cabinet.js";
 import { bdCnnection } from "./scr/db/db.js";
 import cors from "cors";
 
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 auth(app);
-app(app);
+get(app);
 
 app.listen(process.env.PORT || 3000, function () {
   console.log(
--- scr/personal/personal-cabinet.js
+++ scr/personal/personal-cabinet.js
@@ -1,5 +1,3 @@
-const express = require("express");
-const app = express();
 const { User } = require("../models");
 const jwt = require("jsonwebtoken");
 
