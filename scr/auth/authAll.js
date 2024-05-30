e9d02749167ac50b861cc28ff4077735ac692fae
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2024-05-30 14:42:57 +0600

    ilim

--- scr/auth/authAll.js
+++ scr/auth/authAll.js
@@ -1,6 +1,7 @@
 import User from "../model/order.js";
 import validator from "validator";
 import jwt from "jsonwebtoken";
+import authenticate from "./authenticate.js"; // Импортируйте middleware
 
 const jwtToken = (id) => {
   return jwt.sign({ _id: id }, "secret", { expiresIn: "1h" });
@@ -9,7 +10,7 @@ const jwtToken = (id) => {
 export const auth = (app) => {
   app.post("/register", async (req, res) => {
     try {
-      const { email, oldPassword, password } = req.body;
+      const { email, confirm_password, password } = req.body;
       if (!email || !validator.isEmail(email)) {
         return res.status(400).json({ error: "Некорректный email" });
       }
@@ -18,7 +19,7 @@ export const auth = (app) => {
           .status(400)
           .json({ error: "Пароль должен содержать не менее 8 символов" });
       }
-      if (password !== oldPassword) {
+      if (password !== confirm_password) {
         return res.status(400).json({ error: "Пароли не совпадают" });
       }
       const isUser = await User.findOne({ email });
@@ -60,19 +61,29 @@ export const auth = (app) => {
     }
   });
 
-  app.post("/activation", async (req, res) => {
+  app.patch("/new_password", authenticate, async (req, res) => {
     try {
-      const { email, password } = req.body;
-      const user = await User.findOne({ email });
-      if (!user || user.password !== password) {
-        return res.status(401).json({ error: "Invalid username or password" });
+      const { old_password, password, confirm_password } = req.body;
+      if (!password || !validator.isLength(password, { min: 8 })) {
+        return res
+          .status(400)
+          .json({ error: "Пароль должен содержать не менее 8 символов" });
       }
+      if (password !== confirm_password) {
+        return res.status(400).json({ error: "Пароли не совпадают" });
+      }
+      const user = await User.findOne({ _id: req.user._id });
+      if (!user || user.password !== old_password) {
+        return res.status(401).json({ error: "Invalid old password" });
+      }
+      user.password = password;
+      await user.save();
       res.status(200).json({
         response: true,
-        message: "Вход в систему успешно выполнен",
+        message: "Пароль успешно обновлен",
       });
     } catch (error) {
-      console.error("Error logging in:", error);
+      console.error("Error updating password:", error);
       res.status(500).json({ error: "Internal server error" });
     }
   });
