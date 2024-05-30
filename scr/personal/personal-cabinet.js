364d72391fde008b4bde15aa151f9fb86689e5f6
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2024-05-30 13:05:38 +0600

    ilim

--- scr/personal/personal-cabinet.js
+++ scr/personal/personal-cabinet.js
@@ -9,13 +9,14 @@ const authenticate = (req, res, next) => {
 };
 
 export const personal = (app) => {
-  app.get("/personal-cabinet", async (req, res) => {
+  app.get("/personal-cabinet", authenticate, async (req, res) => {
     try {
       const user = await User.findOne({ _id: req.user._id });
       res.status(200).json({ user });
+      return res.status(404).json({ error: "User not found" });
     } catch (error) {
       console.error("Error fetching user:", error);
-      return res.status(404).json({ error: "User not found" });
+      return res.status(401).json({ error: "Токен жоок" });
     }
   });
 };
