89c5508a4e90cf17cdb71bd7847904e7b84a7aa6
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2024-05-30 18:45:30 +0600

    ilim

--- scr/model/order.js
+++ scr/model/order.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
-  confirm_password: { type: String, required: true },
 });
 
 const User = mongoose.model("users", userSchema);
