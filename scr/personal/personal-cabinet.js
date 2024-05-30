12541308b5e8d72a4c4f799e58bf3d6868557c7c
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2024-05-30 13:13:33 +0600

    ilim

--- scr/personal/personal-cabinet.js
+++ scr/personal/personal-cabinet.js
@@ -2,9 +2,18 @@ import User from "../model/order.js";
 import jwt from "jsonwebtoken";
 
 const authenticate = (req, res, next) => {
-  const token = req.header("Authorization").replace("Bearer ", "");
+  const token = req.header("Authorization")?.replace("Bearer ", "");
   if (!token) {
     return res.status(401).json({ error: "Access denied. No token provided." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, "secret");
+    req.user = decoded;
+    next();
+  } catch (err) {
+    console.error("Token verification failed:", err);
+    res.status(400).json({ error: "Invalid token." });
   }
 };
 
