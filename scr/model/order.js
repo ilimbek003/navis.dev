1193bcf54357a81acaf5a51f80f1adfc20adcc56
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2024-05-30 12:47:27 +0600

    commit

--- scr/model/order.js
+++ scr/model/order.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  oldPassword: String,
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  oldPassword: { type: String, required: true },
 });
 
 const User = mongoose.model("users", userSchema);
