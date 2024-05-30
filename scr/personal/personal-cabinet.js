7d428877fb156031f835f7f8aea233756bbe7f32
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2024-05-30 12:58:21 +0600

    ilim

--- scr/personal/personal-cabinet.js
+++ scr/personal/personal-cabinet.js
@@ -1,20 +1,12 @@
 import User from "../model/order.js";
-// import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
-// const authenticate = (req, res, next) => {
-//   const token = req.header("Authorization").replace("Bearer ", "");
-//   if (!token) {
-//     return res.status(401).json({ error: "Access denied. No token provided." });
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, "secret");
-//     req.user = decoded;
-//     next();
-//   } catch (err) {
-//     res.status(400).json({ error: "Invalid token." });
-//   }
-// };
+const authenticate = (req, res, next) => {
+  const token = req.header("Authorization").replace("Bearer ", "");
+  if (!token) {
+    return res.status(401).json({ error: "Access denied. No token provided." });
+  }
+};
 
 export const personal = (app) => {
   app.get("/personal-cabinet", async (req, res) => {
