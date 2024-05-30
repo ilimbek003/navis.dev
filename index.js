3fc98718f2637dd4ee0bf9bc673a0d7996c7623c
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2024-05-30 17:48:18 +0600

    ilim

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 auth(app);
 personal(app);
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 4000, function () {
   console.log(
     "Express server listening on port %d in %s mode",
     this.address().port,
