d04b28069bcc677a91dde3c2f82ab80e6cf0244f
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2024-05-30 18:09:07 +0600

    ilim

--- scr/auth/authAll.js
+++ scr/auth/authAll.js
@@ -1,8 +1,6 @@
 import User from "../model/order.js";
 import validator from "validator";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import { authenticate } from "../token/authenticate.js";
 
 const jwtToken = (id) => {
   return jwt.sign({ _id: id }, "secret", { expiresIn: "1h" });
@@ -11,7 +9,7 @@ const jwtToken = (id) => {
 export const auth = (app) => {
   app.post("/register", async (req, res) => {
     try {
-      const { email, password, confirm_password } = req.body;
+      const { email, oldPassword, password } = req.body;
       if (!email || !validator.isEmail(email)) {
         return res.status(400).json({ error: "Некорректный email" });
       }
@@ -20,7 +18,7 @@ export const auth = (app) => {
           .status(400)
           .json({ error: "Пароль должен содержать не менее 8 символов" });
       }
-      if (password !== confirm_password) {
+      if (password !== oldPassword) {
         return res.status(400).json({ error: "Пароли не совпадают" });
       }
       const isUser = await User.findOne({ email });
@@ -29,8 +27,7 @@ export const auth = (app) => {
           .status(409)
           .json({ error: "Пользователь с таким email уже существует" });
       }
-      const hashedPassword = await bcrypt.hash(password, 10); 
-      const newUser = new User({ email, password: hashedPassword });
+      const newUser = new User({ email, password });
       await newUser.save();
 
       res.status(201).json({
@@ -47,7 +44,7 @@ export const auth = (app) => {
     try {
       const { email, password } = req.body;
       const user = await User.findOne({ email });
-      if (!user || !(await bcrypt.compare(password, user.password))) {
+      if (!user || user.password !== password) {
         return res.status(401).json({ error: "Invalid username or password" });
       }
       const token = jwtToken(user._id);
@@ -62,30 +59,21 @@ export const auth = (app) => {
       res.status(500).json({ error: "Internal server error" });
     }
   });
-  //   app.patch("/new_password", authenticate, async (req, res) => {
-  //     try {
-  //       const { old_password, password, confirm_password } = req.body;
-  //       if (!password || !validator.isLength(password, { min: 8 })) {
-  //         return res
-  //           .status(400)
-  //           .json({ error: "Пароль должен содержать не менее 8 символов" });
-  //       }
-  //       if (password !== confirm_password) {
-  //         return res.status(400).json({ error: "Пароли не совпадают" });
-  //       }
-  //       const user = await User.findOne({ _id: req.user._id });
-  //       if (!user || user.password !== old_password) {
-  //         return res.status(401).json({ error: "Неверный старый пароль" });
-  //       }
-  //       user.password = password;
-  //       await user.save();
-  //       res.status(200).json({
-  //         response: true,
-  //         message: "Пароль успешно обновлен",
-  //       });
-  //     } catch (error) {
-  //       console.error("Ошибка обновления пароля:", error);
-  //       res.status(500).json({ error: "Внутренняя ошибка сервера" });
-  //     }
-  //   });
+
+  app.patch("/activation", async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user || user.password !== password) {
+        return res.status(401).json({ error: "Invalid username or password" });
+      }
+      res.status(200).json({
+        response: true,
+        message: "Вход в систему успешно выполнен",
+      });
+    } catch (error) {
+      console.error("Error logging in:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
 };
--- scr/model/order.js
+++ scr/model/order.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
-  confirm_password: { type: String, required: true },
-  old_password: { type: String, required: true },
+  oldPassword: { type: String, required: true },
 });
 
 const User = mongoose.model("users", userSchema);
