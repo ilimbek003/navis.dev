519094c0ffb81aa6a3f662be738636593a945da3
ilimbek003 <kaiyrbekuulualumbek@gmail.com>
2024-05-30 12:53:20 +0600

    caommit

--- scr/personal/personal-cabinet.js
+++ scr/personal/personal-cabinet.js
@@ -1,23 +1,23 @@
 import User from "../model/order.js";
-import jwt from "jsonwebtoken";
+// import jwt from "jsonwebtoken";
 
-const authenticate = (req, res, next) => {
-  const token = req.header("Authorization").replace("Bearer ", "");
-  if (!token) {
-    return res.status(401).json({ error: "Access denied. No token provided." });
-  }
+// const authenticate = (req, res, next) => {
+//   const token = req.header("Authorization").replace("Bearer ", "");
+//   if (!token) {
+//     return res.status(401).json({ error: "Access denied. No token provided." });
+//   }
 
-  try {
-    const decoded = jwt.verify(token, "secret");
-    req.user = decoded;
-    next();
-  } catch (err) {
-    res.status(400).json({ error: "Invalid token." });
-  }
-};
+//   try {
+//     const decoded = jwt.verify(token, "secret");
+//     req.user = decoded;
+//     next();
+//   } catch (err) {
+//     res.status(400).json({ error: "Invalid token." });
+//   }
+// };
 
 export const personal = (app) => {
-  app.get("/personal-cabinet", authenticate, async (req, res) => {
+  app.get("/personal-cabinet", async (req, res) => {
     try {
       const user = await User.findOne({ _id: req.user._id });
       if (!user) {
